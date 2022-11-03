c22e3476505358c16c521a2bf3dc0f45695811f7
maurya-subhashini1 <82706122+maurya-subhashini1@users.noreply.github.com>
2022-11-03 02:41:43 -0700

    Delete con.js

--- db/con.js
+++
@@ -1,8 +0,0 @@
-// const mongoose=require("mongoose");
-
-// mongoose.connect("mongodb://localhost:27017/mydatabase")
-// .then(()=>{
-//     console.log("connection successful");
-// }).catch(()=>{
-//     console.log("No connection");
-// })
